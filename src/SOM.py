179e20ad368e22d8ef9a64a4c61983d761cbef22
Dane Cross <dane.cross2016@gmail.com>
2024-06-13 07:29:40 -0700

    Use faster classifier

--- src/SOM.py
+++ src/SOM.py
@@ -7,11 +7,12 @@ import tqdm
 import os
 
 from NoiseSOM import *
+from SOMRunner import ClassifyRunner
 
 class SOM(object):
 
-	def __init__(self, resolution, train_cat, 
-						analysis_output_path='./',
+	def __init__(self, resolution, train_cat=None, pretrained_weights=None, 
+						reinit_SOM_args={}, analysis_output_path='./',
 						col_fmt="Mf_", cov_fmt="cov_Mf_", err_fmt=None):
 		'''
 		Initialize a SOM object (wraps Gary's NoiseSOM)
@@ -19,6 +20,9 @@ class SOM(object):
 		args: 
 			- resolution (int): the square side size of the SOM
 			- train_cat (str): path to the randomly selected catalog for training the SOM
+			- pretrained_weights (numpy ndarray): a resolutionxresolution numpy array of 
+															  pre-trained weights
+			- reinit_SOM_args (dict): arguments for the NoiseSOM metric
 			- *analysis_output_path (str): directory to save analysis ouptuts
 			- *col_fmt (str): format for the flux columns the catalog
 			- *cov_fmt (str): format for the flux covariance columns in the catalog (if errors 
@@ -27,22 +31,55 @@ class SOM(object):
 					if flux errors, set this value appropriately)
 
 		Note on flux column names: the [col/cov/err]_fmt arguments should denote the beginning 
-			of the flux column names. e.g. if the col_fmt = "EX_FLUX_" then the column for the  
+			of the flux column names. e.g. if the col_fmt = "EX_FLUX_" then the column for the	
 			g-band should be "EX_FLUX_G" or EX_FLUX_g""
 
 		'''
 
 		self.save_path = os.path.abspath(analysis_output_path)
 		self.somres = resolution
-		self.train_cat_path = os.path.abspath(train_cat)
 			
-		self._get_catalogs(train_cat, col_fmt, cov_fmt, err_fmt)
-		self.SOM = self._initialize_SOM(self.train_fluxes, self.train_err)
-		self.trained = False
+		if train_cat is not None:
+			self._get_catalogs(train_cat, col_fmt, cov_fmt, err_fmt)
+			self.train_cat_path = os.path.abspath(train_cat)
+			self.SOM = self._initialize_SOM(self.train_fluxes, self.train_err)
+			self.trained = False
+		elif pretrained_weights is not None:
+			self.SOM = self._reinitialize_SOM(pretrained_weights, **reinit_SOM_args)
+			self.trained = True
+		else:
+			raise ValueError("Either train_cat or pretrained_weights must be not None")
 
 		self.col_fmt = col_fmt
 		self.cov_fmt = cov_fmt
 		self.err_fmt = err_fmt
+
+	def _initialize_SOM(self, fluxes, fluxes_err):
+
+		if len(self.train_sample) == 0: return
+		
+		nTrain=fluxes.shape[0]
+		hh = hFunc(nTrain,sigma=(30,1))
+		metric = AsinhMetric(lnScaleSigma=0.4,lnScaleStep=0.03)
+		indices = np.random.choice(fluxes.shape[0],size=nTrain,replace=False)
+		som = NoiseSOM(metric,fluxes[indices,:],fluxes_err[indices,:], \
+									 learning=hh, \
+									 shape=(self.somres,self.somres), \
+									 wrap=False,logF=True, \
+									 initialize='sample', \
+									 minError=0.02)
+
+		return som
+
+	def _reinitialize_SOM(self, som_weights, hFunc_sigma = (30, 1), lnScaleSigma = 0.4,  
+									lnScaleStep = 0.03, minError = 0.02):
+
+		metric  = AsinhMetric(lnScaleSigma = lnScaleSigma, lnScaleStep = lnScaleStep)
+		som     = NoiseSOM(metric, None, None, learning = None, shape = som_weights.shape[:-1],
+                       	 wrap=False, logF=True, initialize=som_weights, minError = minError)
+	
+		return som
+
 
 	def _get_catalogs(self, train_path, col_fmt, cov_fmt, err_fmt):
 
@@ -105,23 +142,6 @@ class SOM(object):
 
 		return flux_cols, coverr_cols
 
-	def _initialize_SOM(self, fluxes, fluxes_err):
-
-		if len(self.train_sample) == 0: return
-		
-		nTrain=fluxes.shape[0]
-		hh = hFunc(nTrain,sigma=(30,1))
-		metric = AsinhMetric(lnScaleSigma=0.4,lnScaleStep=0.03)
-		indices = np.random.choice(fluxes.shape[0],size=nTrain,replace=False)
-		som = NoiseSOM(metric,fluxes[indices,:],fluxes_err[indices,:], \
-                            learning=hh, \
-                            shape=(self.somres,self.somres), \
-                            wrap=False,logF=True, \
-                            initialize='sample', \
-                            minError=0.02)
-
-		return som
-
 	def train(self):
 		self.SOM.train()
 		if self.save_path is not None:
@@ -146,71 +166,16 @@ class SOM(object):
 			assignments = pickle.load(f)
 		return assignments
 
-	def _run_assignments(self, table, num_inds=1000, save=True, num_threads=50,
-					 			col_fmt=None, err_fmt=None, cov_fmt=None):
+	def classify(self, table, num_threads=150, num_inds=100000, savepth=None,
+					 flux_fmt=None, err_fmt=None, cov_fmt=None, ID_fmt='ID'):
 
-		'''
-		Assign fluxes to cells in trained SOM. 
-		
-		Args: 
-			- *table (astropy.Table): table to classify. 
-			- *num_inds (int): number of subtables to split the main table into
-			- *save (str, bool): flag for saving the assignments. If True, will save to the 
-										default save path in a file called "assignments.pkl". If 
-										argument is a path, will save to that path.
-			- num_threads (int): number of multiprocess threads to run the classifications on
-			- col_fmt (str): format of the flux columns. If None, defaults to SOM format.
-			- err_fmt (str): format of the error columns. If None, defaults to SOM format.
-			- cov_fmt (str): format of the covariance columns. If None, defaults to SOM format.
-		'''
-
-		flx_cn, fer_cn = self._get_column_names(table, col_fmt, cov_fmt, err_fmt)
+		flx_cn, fer_cn = self._get_column_names(table, flux_fmt, cov_fmt, err_fmt)
 		fluxes = self._get_columns(table, flx_cn)
 		if err_fmt is not None: errs = self._get_columns(table, fer_cn)
 		else: errs = np.sqrt(self._get_columns(table, fer_cn))
 
-		def assign_som(classify_func, fluxes, errs):
-			cells, _ = classify_func(fluxes, errs)
-			del fluxes
-			del errs
-			return cells
-   
-		num_inds = min(len(fluxes), num_inds)
-		inds = np.array_split(np.arange(len(fluxes)),num_inds)
-		args = [(self.SOM.classify, fluxes[inds[index]], errs[inds[index]],) for index in range(num_inds)]
-		with mp.Pool() as p: 
-			results = p.starmap(assign_som, tqdm.tqdm(args, total=len(args)))
+		return ClassifyRunner(fluxes, errs, table[ID_fmt], self.SOM.weights)
 
-		assignments = []
-		for res in results: assignments = np.append(assignments,res)
-      
-		if save:
-			fpath = os.path.join(self.save_path, "assignments.pkl") if type(save) != str else save
-			with open(fpath, 'wb') as f:
-				pickle.dump(assignments, f)
-
-		return assignments
-
-	def classify(self, table, num_threads=150, num_inds=100000, savepth=None,
-					 flux_fmt=None, err_fmt=None, cov_fmt=None):
-
-		if flux_fmt is None and err_fmt is None and cov_fmt is None:
-			flux_colname = self.col_fmt 
-			flux_err_colname = self.err_fmt 
-			flux_cov_colname = self.cov_fmt
-		else:
-			flux_colname = flux_fmt
-			flux_err_colname = err_fmt
-			flux_cov_colname = cov_fmt
-
-		assignments = self._run_assignments(table=table, save=savepth, 
-														num_threads=num_threads, num_inds=num_inds,
-														col_fmt=flux_colname, 
-														err_fmt=flux_err_colname, 
-														cov_fmt=flux_cov_colname)
-
-		return assignments
-		
 
 	def save(self, savepath='.'):
 		
